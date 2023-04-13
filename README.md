23539051b9ddea2c01524c6c875b29146dd9d5e8
Thomas <tad85@pitt.edu>
2023-04-13 15:44:54 -0400

    search for genre and title

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # CS1530_project
 
-##Startup instructions
+## Startup instructions
+
 make sure you have python and run pip install -r requirements.txt
+
 run:
+```
 set FLASK_APP=app.py
 set FLASK_ENV=development
 py -m flask run
-
+```
 init database with: py -m flask initdb (currently does nothing)
--- app.py
+++ app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'development key'
 
-
-
 class User():
     def __init__(self, username, password, email, creationTime = None):
         self.username = username
@@ -35,58 +33,105 @@ class Review():
         self.rating = rating 
         self.review = review
 
+class Game():
+    def __init__(self, title, desc, genreid):
+        self.title = title
+        self.desc = desc
+        self.genre = "None"
+        for key in Database.genre_mappings.keys():
+            if Database.genre_mappings[key] in genreid:
+                if self.genre == "None":
+                    self.genre = key
+                else:
+                    self.genre += (', ' + key)
+
+    def fromDB(data_row):
+        genreids = [int(val) for val in data_row[12].strip('][').split(', ')] if data_row[12] != 'None' else []
+        return Game(data_row[1], data_row[6], genreids)
+        
 class Database():
-    
-    def addUser(self, userObject):
-        ### db implementation:
-        con = sqlite3.connect('database.db') 
-        cur = con.cursor()
+    genre_mappings = { # maps a readable genre to the genre id number, currently genre names are g_ as i dont know what the mapping is
+        'g1' : 1,
+        'g2' : 2,
+        'g3' : 3,
+        'g4' : 4,
+        'g5' : 5,
+        'g6' : 6,
+        'g7' : 7,
+        'g8' : 8,
+        'g9' : 9,
+        'g10' : 10,
+        'g11' : 11,
+        'g12' : 12,
+        'g13' : 13,
+        'g14' : 14,
+        'g15' : 15,
+        'g16' : 16,
+        'g17' : 17,
+        'g18' : 18,
+        'g19' : 19,
+    }
 
-        query = """INSERT INTO users(user_id, email, password, join_date) 
-                   VALUES('{}','{}','{}','{}')""".format(userObject.username, userObject.email, userObject.password, datetime.today())
-        cur.execute(query)
-        con.commit()
-        con.close()
-
-    def getUser(self, username):
+    def get(query):
         con = sqlite3.connect('database.db') 
         cur = con.cursor() 
-        
-        query="""SELECT * FROM users WHERE user_id = '{}'""".format(username)
         cur.execute(query)
         all_rows = cur.fetchall()
         con.close()
         if (len(all_rows) == 0): # Username not found in database
             return None
         else:
-            return User.fromDB(all_rows[0])
+            return all_rows
 
-    def getReviews(self, gameName):
-        con = sqlite3.connect('database.db') 
-        cur = con.cursor() 
-        
-        query="""SELECT * FROM reviews WHERE gameName = '{}'""".format(gameName)
-        cur.execute(query)
-        all_rows = cur.fetchall()
-        con.close()
-        if (len(all_rows) == 0): # Username not found in database
-            return []
-        else:
-            return [Review(*data_row) for data_row in all_rows]
-
-    def postReview(self, reviewObject):
+    def post(query):
         con = sqlite3.connect('database.db') 
         cur = con.cursor()
 
-        query = """INSERT INTO reviews(gameName, user, date, rating, review) 
-                   VALUES('{}','{}','{}','{}','{}')""".format(reviewObject.gameName, reviewObject.user.username, datetime.today(), reviewObject.rating, reviewObject.review)
         cur.execute(query)
         con.commit()
         con.close()
 
+    
+    def addUser(self, userObject):
+        query = """INSERT INTO users(user_id, email, password, join_date) 
+                   VALUES('{}','{}','{}','{}')""".format(userObject.username, userObject.email, userObject.password, datetime.today())
+        Database.post(query)
+
+    def getUser(self, username):
+        query="""SELECT * FROM users WHERE user_id = '{}'""".format(username)
+        all_rows = Database.get(query)
+        return User.fromDB(all_rows[0]) if len(all_rows) > 0 else None
+
+    def getReviews(self, gameName):
+        query="""SELECT * FROM reviews WHERE gameName = '{}'""".format(gameName)
+        all_rows = Database.get(query)
+        return [Review(*data_row) for data_row in all_rows] if len(all_rows) > 0 else []
+
+    def postReview(self, reviewObject):
+        query = """INSERT INTO reviews(gameName, user, date, rating, review) 
+                   VALUES('{}','{}','{}','{}','{}')""".format(reviewObject.gameName, reviewObject.user.username, datetime.today(), reviewObject.rating, reviewObject.review)
+        Database.post(query)
+
+    def getGenre(self, genreid):
+        query=F"""SELECT * FROM games WHERE genres LIKE '%{genreid}%' LIMIT 100"""
+        all_rows = Database.get(query)
+
+        for data_row in all_rows: # the search query will also return things like genre = 12 when searching for genre = 1 due to the way string matching works
+            genreids = [int(val) for val in data_row[12].strip('][').split(', ')] if data_row[12] != 'None' else []
+            if int(genreid) not in genreids:
+                all_rows.remove(data_row)
+
+        return [Game.fromDB(data_row) for data_row in all_rows][:50] if len(all_rows) > 0 else []
+
+    def getGame(self, title):
+        query=F"""SELECT * FROM games WHERE game_title LIKE '%{title.lower()}%'"""
+        all_rows = Database.get(query)
+        return [Game.fromDB(data_row) for data_row in all_rows] if len(all_rows) > 0 else []
+
     def deleteAllUsers(self):
         print("TODO: implement Database.deleteAllUsers")
-        
+
+    
         
 def hash(password):
     return hashlib.sha256(password.encode()).hexdigest()
@@ -161,6 +206,41 @@ def register():
             flash('You were successfully registered and can login now')
             return redirect(url_for('login'))
     return render_template('register.html', error=error, header="Sign Up")
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    query = request.args.get('search', None)
+    if query == None:
+        return redirect('/')
+
+    if query.startswith('genre:'):
+        return redirect(F'/search/genre/{query.replace("genre:", "", 1)}')
+        
+    return redirect(F'/search/title/{query}')
+
+@app.route('/search/genre/<path:genre>', methods=['GET'])
+def search_genre(genre):
+    genreid = Database.genre_mappings[genre.lower()]
+    games = db.getGenre(genreid)
+    games = removeDuplicates(games)
+    return render_template('search_results.html', header = genre, games = games)
+
+@app.route('/search/title/<path:title>', methods=['GET'])
+def search_title(title):
+    games = db.getGame(title)
+    games = removeDuplicates(games)
+    return render_template('search_results.html', header = title, games = games)
+
+def removeDuplicates(games):
+    hashTable = {}
+
+    for game in games:
+        if game.title.lower() not in hashTable:
+            hashTable[game.title.lower()] = game
+
+    return hashTable.values()
+
+
 
 @app.route('/game/<path:gameName>', methods=['GET', 'POST'])
 def game(gameName):
